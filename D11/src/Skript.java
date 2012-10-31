29bdae62497174f18fd5f72ca0bd4bb6bf063974
Gudny <gudnyg11@ru.is>
2012-10-31 18:52:51 +0000

    project added

---
+++ D11/src/Skript.java
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Guðný
+ * Date: 10/31/12
+ * Time: 6:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Guðný
+ * Date: 10/31/12
+ * Time: 6:37 PM
+ *
+ * @author Guðný Björk Gunnarsdóttir
+ */
+public class Skript {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
